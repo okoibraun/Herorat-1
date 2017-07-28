0e731b28b3105aa4311910e37f12d8310869edba
steven2805 <stevenmac90@googlemail.com>
2017-07-28 11:28:41 +0100

    completed the basics of the hero along with the passing tests

--- hero.js
+++ hero.js
@@ -1,6 +1,14 @@
-var Hero = function(name){
+var Hero = function(name, favfood, speak){
   this.name = name;
+  this.health = 100;
+  this.favfood = favfood;
+  this.speak = this.name + " is my name";
+  this.tasks = [];
 }
 
+Hero.prototype.tastlistlength = function(){
+  return this.tasks.length;
+};
 
-module.exports = Hero;''
+
+module.exports = Hero;
--- spec/hero_spec.js
+++ spec/hero_spec.js
@@ -4,7 +4,7 @@ var Hero = require('../hero.js')
 describe('Hero Testing', function(){
 
   beforeEach(function(){
-    hero1 = new Hero("Steven")
+    hero1 = new Hero("Steven","Apples")
   })
 
 
@@ -15,4 +15,27 @@ describe('Hero Testing', function(){
   it('should have 100 health at start', function(){
     assert.strictEqual(hero1.health, 100)
   })
+
+  it("should have fav food",function(){
+    assert.strictEqual(hero1.favfood , "Apples")
+  })
+
+  it('should have a name', function(){
+    assert.strictEqual(hero1.speak, "Steven is my name")
+  })
+
+  it("Should start task list empty", function(){
+    assert.strictEqual(hero1.tastlistlength(),0)
+  })
+
+
+
+
+
+
+
+
+
+
+
 })
